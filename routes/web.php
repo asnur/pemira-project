1e9a86587cfb04132c9cb63480e3c35cc131d8e9
sabiq7392 <sabiqmuhammad@gmail.com>
2022-02-20 16:28:32 +0700

    bantai all anjay

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CekDaftarPemilihTetapController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\KandidatController;
-use App\Http\Controllers\LiveCountController;
-use App\Http\Controllers\VoteController;
+use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\VotingController;
+use App\Http\Controllers\KandidatController;
+use App\Models\Kandidat;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -26,10 +26,10 @@ Auth::routes();
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/voting', [VotingController::class, 'index']);
-Route::get('/auth/login', [AuthController::class, 'login']);
-Route::get('/auth/register', [AuthController::class, 'register']);
-Route::get('/auth/register-success', [AuthController::class, 'registerSuccess']);
-Route::get('/cek-daftar-pemilih-tetap', [CekDaftarPemilihTetapController::class, 'index']);
-Route::get('/vote', [VoteController::class, 'index']);
-Route::get('/live-count', [LiveCountController::class, 'index']);
-Route::get('/kandidat', [KandidatController::class, 'index']);
+Route::prefix('/admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index']);
+    Route::resource('/mahasiswa', MahasiswaController::class);
+    Route::get('/voting', [VotingController::class, 'index']);
+    Route::resource('/kandidat', KandidatController::class);
+});
+
