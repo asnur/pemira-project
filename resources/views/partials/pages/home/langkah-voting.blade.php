46c88763bac7cbbcbcd1a27623cbaa21cb8959d9
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-03-23 09:21:12 +0700

    update video cara voting

--- resources/views/partials/pages/home/langkah-voting.blade.php
+++ resources/views/partials/pages/home/langkah-voting.blade.php
@@ -1,48 +1,58 @@
 <div class="mb-5">
-  <div class="container">
-    <h1 class="fw-bold text-color mt-5 mb-5 text-center">Langkah Langkah Voting</h1>
-    <div class="row align-items-center justify-content-center">
-      <div class="col-lg-2 text-center step d-flex align-items-center p-0">
-        <div class="card step-guide h-100 border-0">
-          <div class="card-body p-0">
-            <img src="{{ asset('/img/Mobile login-pana.png') }}" class="img-guide  w-100" />
-            <h4 class="text-color mt-2 fw-bold">Daftar Ulang</h4>
-          </div>
+    <div class="container">
+        <h1 class="fw-bold text-color mt-5 mb-5 text-center">Langkah Langkah Voting</h1>
+        <div class="row align-items-center justify-content-center">
+            <div class="col-lg-2 text-center step d-flex align-items-center p-0">
+                <div class="card step-guide h-100 border-0">
+                    <div class="card-body p-0">
+                        <img src="{{ asset('/img/Mobile login-pana.png') }}" class="img-guide  w-100" />
+                        <h4 class="text-color mt-2 fw-bold">Daftar Ulang</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1 d-flex align-items-center">
+                <div class="foot-path"></div>
+            </div>
+            <div class="col-lg-2 text-center step d-flex align-items-center p-0">
+                <div class="card step-guide border-0">
+                    <div class="card-body p-0">
+                        <img src="{{ asset('/img/Reset password-bro.png') }}" class="img-guide w-100" />
+                        <h4 class="text-color mt-2 fw-bold">Login</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1 d-flex align-items-center">
+                <div class="foot-path"></div>
+            </div>
+            <div class="col-lg-2 text-center step d-flex align-items-center p-0">
+                <div class="card step-guide border-0">
+                    <div class="card-body p-0">
+                        <img src="{{ asset('/img/Voting-bro.png') }}" class="img-guide w-100" />
+                        <h4 class="text-color mt-2 fw-bold">Voting</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1 d-flex align-items-center">
+                <div class="foot-path"></div>
+            </div>
+            <div class="col-lg-2 text-center step p-0 d-flex align-items-center">
+                <div class="card step-guide border-0">
+                    <div class="card-body p-0">
+                        <img src="{{ asset('/img/Done-pana.png') }}" class="img-guide w-100" />
+                        <h4 class="text-color mt-2 fw-bold">Selesai</h4>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-lg-1 d-flex align-items-center">
-        <div class="foot-path"></div>
-      </div>
-      <div class="col-lg-2 text-center step d-flex align-items-center p-0">
-        <div class="card step-guide border-0">
-          <div class="card-body p-0">
-            <img src="{{ asset('/img/Reset password-bro.png') }}" class="img-guide w-100" />
-            <h4 class="text-color mt-2 fw-bold">Login</h4>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-1 d-flex align-items-center">
-        <div class="foot-path"></div>
-      </div>
-      <div class="col-lg-2 text-center step d-flex align-items-center p-0">
-        <div class="card step-guide border-0">
-          <div class="card-body p-0">
-            <img src="{{ asset('/img/Voting-bro.png') }}" class="img-guide w-100" />
-            <h4 class="text-color mt-2 fw-bold">Voting</h4>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-1 d-flex align-items-center">
-        <div class="foot-path"></div>
-      </div>
-      <div class="col-lg-2 text-center step p-0 d-flex align-items-center">
-        <div class="card step-guide border-0">
-          <div class="card-body p-0">
-            <img src="{{ asset('/img/Done-pana.png') }}" class="img-guide w-100" />
-            <h4 class="text-color mt-2 fw-bold">Selesai</h4>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 </div>
+
+{{-- <div class="container mb-4">
+    <center>
+        <div class="w-50 ratio ratio-16x9">
+            <iframe src="https://www.youtube.com/embed/UpPklv4zLfc" title="YouTube video player" frameborder="0"
+                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+        </div>
+    </center>
+</div> --}}
